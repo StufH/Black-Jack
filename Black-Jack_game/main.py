24ce953a7f68d8be82b35b34116ea9c08d03386e
Henrik Stueland <henrikstueland@gmail.com>
2025-04-02 08:43:23 +0200

    refactor: update deck initialization and remove unused input code

--- Black-Jack_game/main.py
+++ Black-Jack_game/main.py
@@ -5,22 +5,36 @@ import random as rd
 
 
 
-deck_size = input("How many decks do you want to play with? (1-8) ") # How many decks do you want to play with
-for i in range(0,int(deck_size)):
-    gamedeck.append(deck_size)
-
-print("The total number of cards in the deck is: ", len(gamedeck))
+# deck_size = input("How many decks do you want to play with? (1-8) ") # How many decks do you want to play with
+# for i in range(0,int(deck_size)):
+#     gamedeck.append(deck_size)
+#
+# print("The total number of cards in the deck is: ", len(gamedeck))
 
 
 class Deck:
-    def __init__(self, type, card, amount):
+    def __init__(self, type, card, value):
         self.type = type
         self.card = card
-        self.amount = amount
+        self.value = value
+
 
 gamedeck = []
-for i in range(4):
 
+for i in range(0, 4):
+    for j in range(1, 10):
+        if i == 0:
+            gamedeck.append(Deck("Hearts", j , j))
+        if i == 1:
+            gamedeck.append(Deck("Spades", j, j))
+        if i == 0:
+            gamedeck.append(Deck("Diamonds", j, j))
+        if i == 1:
+            gamedeck.append(Deck("Clubs", j, j))
+
+
+def kortvelger():
 
 
 def dealer(deck):
+    print()
